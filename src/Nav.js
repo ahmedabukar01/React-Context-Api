13fb25eb3060bb8b40c26f06e017aa4137436824
ahmed <ahmedlearn01@gmail.com>
2022-01-22 13:39:57 -0800

    add total movie

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,11 @@
+import {useContext} from 'react'
+import { MovieContext } from './MoviesContex';
 
 const Nav = () => {
+    const [movies,setMovies] = useContext(MovieContext);
   return(
       <div>
-          <h3>List of movies: </h3>
+          <h3>List of movies: {movies.length} </h3>
       </div>
   )
 };
